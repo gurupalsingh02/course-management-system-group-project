09e9ce85793936b24fc0f4e855a1ab94e0284e40
Akash Mishra <akashmishra242@gmail.com>
2021-11-28 15:58:58 +0530

    static String StudentAccountPage  created

---
+++ lib/Pages/student_account_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:course_management_system/routes.dart';
+
+class StudentAccountPage extends StatelessWidget {
+  const StudentAccountPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: file_names, non_constant_identifier_names
-
 class MyRoutes {
   static String LoginPage = "/Login";
   static String HomePage = "/Home";
   static String ToDoPage = "/Todo";
   static String AccountPage = "/Account";
   static String StudentLoginPage = "/Student/Login";
+  static String StudentAccountPage = "/Student/Account";
 }
