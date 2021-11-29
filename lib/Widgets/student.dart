d30f9811b786e096a8a858519f5b84aedeea933c
Gurupal Singh <gurupalsingh83@gmail.com>
2021-11-29 21:18:36 +0530

    student class updated

--- lib/Widgets/student.dart
+++ lib/Widgets/student.dart
@@ -7,13 +7,13 @@ class Student {
   String name;
   String image;
   String Student_id;
-  List<Assignment> Assigned = [];
+  List<Assignment> MyAssignments = [];
   List<MyClass> myclasses = [];
   Student({
     required this.name,
     required this.image,
     required this.Student_id,
-    required this.Assigned,
+    required this.MyAssignments,
     required this.myclasses,
   });
 }
