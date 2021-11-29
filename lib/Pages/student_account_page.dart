9698d33d1035c533df29977b9a6eacfbface176b
Akash Mishra <akashmishra242@gmail.com>
2021-11-29 20:52:34 +0530

    Made To do page

--- lib/Pages/student_account_page.dart
+++ lib/Pages/student_account_page.dart
@@ -33,7 +33,7 @@ class StudentAccountPage extends StatelessWidget {
                     )),
                 IconButton(
                     onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.HomePage);
+                      Navigator.pushNamed(context, MyRoutes.ToDoPage);
                     },
                     icon: Icon(
                       Icons.person,
--- lib/Pages/to_do_page.dart
+++ lib/Pages/to_do_page.dart
@@ -1,14 +1,49 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-class ToDoPage extends StatelessWidget {
+class ToDoPage extends StatefulWidget {
   const ToDoPage({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoPage> createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this, initialIndex: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: "To-do".text.make(),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: const [
+            //Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: ("Assigned")),
+            Tab(text: ("Missing")),
+            Tab(text: ("Done")),
+          ],
+        ),
+      ),
+      body: TabBarView(controller: _controller, children: const [
+        Text("Assigned task will appear here."),
+        Text("Missing task will appear here."),
+        Text("Done task will appear here."),
+      ]),
+      drawer: Drawer(),
     );
   }
 }
