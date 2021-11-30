7171ebe71b3e5211bbb4a2c7851aa7d81eb674ae
Akash Mishra <akashmishra242@gmail.com>
2021-11-30 12:20:32 +0530

    Done for guru's satisfaction

--- lib/Widgets/asssignments.dart
+++ lib/Widgets/asssignments.dart
@@ -1,6 +1,6 @@
 class Assignment {
   final String heading;
   final String desc;
-  bool done;
-  Assignment({required this.heading, required this.desc, required this.done});
+  bool done = false;
+  Assignment({required this.heading, required this.desc});
 }
